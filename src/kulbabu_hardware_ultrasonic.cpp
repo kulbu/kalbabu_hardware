20df29c75256ca88401b6cecc0414462f854eb78
Mr-Yellow <mr-yellow@mr-yellow.com>
2016-11-20 05:18:44 +0000

    Compiles...

--- src/kulbabu_hardware_ultrasonic.cpp
+++ src/kulbabu_hardware_ultrasonic.cpp
@@ -1,12 +1,25 @@
+//#include <glib.h>
+//#include <glib/gprintf.h>
+//#include <errno.h>
+//#include <string.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include <ros/ros.h>
-#include <sensor_msgs/Range>
+#include <sensor_msgs/Range.h>
 #include <linux/i2c-dev.h>
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "kulbabu_hardware_ultrasonic");
   ros::NodeHandle nh;
 
-  ros::Publisher range_pub = n.advertise<sensor_msgs::Range>("range", 50);
+  ros::Publisher range_pub = nh.advertise<sensor_msgs::Range>("range", 50);
 
   // Setup i2c
   int file;
@@ -22,7 +35,7 @@ int main(int argc, char** argv) {
   }
 
   // TODO: Configurable.
-  int addr = 0x10; /* The I2C address */
+  uint8_t addr = 0x10; /* The I2C address */
 
   if (ioctl(file, I2C_SLAVE, addr) < 0) {
     /* ERROR HANDLING; you can check errno to see what went wrong */
@@ -31,36 +44,42 @@ int main(int argc, char** argv) {
   }
 
   //ros::spin();
-  ros::Time current_time
 
   // TODO: Configurable rate.
   ros::Rate r(10.0);
-  while (n.ok()) {
-    current_time = ros::Time::now();
+  while (nh.ok()) {
+    ros::Time current_time = ros::Time::now();
 
-    uint8_t res;
-
+    //uint8_t res;
     // Read data from i2c
-    res = i2c_smbus_read_word_data(file, reg);
-    if (res < 0) {
+    //res = i2c_smbus_read_word_data(file, addr);
+    //if (res < 0) {
+
+
+    char buf[10];
+    buf[0] = addr;
+
+    if (read(file, buf, 1) != 1) {
       /* ERROR HANDLING: i2c transaction failed */
-      ROS_WARN_STREAM( "I2C failed " << res );
+      ROS_WARN_STREAM( "I2C failed " << buf );
     } else {
       /* res contains the read word */
-      ROS_INFO_STREAM( "I2C read " << res );
+      ROS_INFO_STREAM( "I2C read " << buf );
 
       // https://github.com/ros/common_msgs/blob/indigo-devel/sensor_msgs/msg/Range.msg
       sensor_msgs::Range range_msg;
       range_msg.header.stamp = current_time;
 
-      range_msg.radiation_type = 0; // Ultrasonic
+      range_msg.radiation_type = 0; // Ultrasonic // FIXME: sensor_msgs::Range::ULTRASOUND
       range_msg.field_of_view = 0.261799388; // 15deg in rad (HCSR04)
       range_msg.max_range = 4; // 4m
       range_msg.min_range = 2/100; // 2cm
 
       for (uint8_t x=0;x<8;x++) {
-        sprintf(range_msg.header.frame_id, "range%d", x)
-        range_msg.range = res[x];
+        char name[10];
+        sprintf(name, "range%d", x);
+        range_msg.header.frame_id = name;
+        range_msg.range = buf[x];
         range_pub.publish(range_msg);
       }
     }
